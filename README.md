0d2e4554fa6b85cbab116db52b22b95719c173e6
Mert Emin Kalender <merteminkalender@gmail.com>
2013-12-10 23:02:44 -0800

    Created the page to demonstrate

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dart Drag and Drop using SVG
+This is a basic example showing how to draw, select and move squares around. Double click draws a new square. One click (de)selects squares and click-and-move drags them. While implementing it, [this link](http://svg-whiz.com/svg/DragAndDrop.svg) is followed.
+
+## Issues
+* Mouse is slipping over square if you want to drag fast
+
+## Changelog
+* **v0.1** - Dart drag and drop using SVG is now ready to be downloaded
+
+## Technologies Used
+* [Dart](http://www.dartlang.org)
+
+## License
+The MIT License
---
+++ dart_drag_and_drop_svg.dart
@@ -0,0 +1,83 @@
+import 'dart:html';
+import 'dart:async';
+import 'dart:svg';
+
+class BasicUnit {
+  
+  SvgSvgElement canvas;
+  GElement group;
+  RectElement body;
+  bool dragging;
+  num dragOffsetX, dragOffsetY, width, height;
+  
+  BasicUnit(SvgSvgElement this.canvas, num x, num y, num this.width, num this.height) {
+    this.body = new RectElement();
+    this.body.setAttribute('x', '$x');
+    this.body.setAttribute('y', '$y');
+    this.body.setAttribute('width', '$width');
+    this.body.setAttribute('height', '$height');
+    this.body.classes.add('processing_body');
+    
+    this.body.onMouseDown.listen(select);
+    this.body.onMouseMove.listen(moveStarted);
+    this.body.onMouseUp.listen(moveCompleted);
+    this.body.onMouseLeave.listen(moveCompleted);
+    
+    this.group = new GElement();
+    this.group.append(this.body);
+    
+    this.dragging = false;
+  }
+  
+  void select(MouseEvent e) {
+    e.preventDefault();
+    this.dragging = true;
+    
+    var mouseCoordinates = getMouseCoordinates(e);
+    this.dragOffsetX = mouseCoordinates['x'] - body.getCtm().e; //double.parse(body.attributes['x']);
+    this.dragOffsetY = mouseCoordinates['y'] - body.getCtm().f;
+  }
+  
+  void moveStarted(MouseEvent e) {
+    e.preventDefault();
+    
+    if (dragging) {
+      var mouseCoordinates = getMouseCoordinates(e);
+      num newX = mouseCoordinates['x'] - dragOffsetX;
+      num newY = mouseCoordinates['y'] - dragOffsetY;
+      this.body.setAttribute('transform', 'translate($newX, $newY)');
+    }
+  }
+  
+  void moveCompleted(MouseEvent e) {
+    e.preventDefault();
+    this.dragging = false;
+  }
+  
+  dynamic getMouseCoordinates(e) {
+    return {'x': (e.offset.x - this.canvas.currentTranslate.x)/this.canvas.currentScale, 
+            'y': (e.offset.y - this.canvas.currentTranslate.y)/this.canvas.currentScale};
+  }
+}
+
+class Application {
+  int WIDTH = 80, HEIGHT = 60;
+  
+  SvgSvgElement canvas;
+  
+  Application(canvas_id) {
+    this.canvas = document.querySelector(canvas_id);
+    this.canvas.onDoubleClick.listen((MouseEvent e) => addNewUnit(e));
+  }
+  
+  void addNewUnit(MouseEvent e) {
+    num x = e.offset.x - WIDTH/2;
+    num y = e.offset.y - HEIGHT/2;
+    BasicUnit newUnit = new BasicUnit(this.canvas, x, y, WIDTH, HEIGHT);
+    this.canvas.append(newUnit.group);
+  }
+}
+
+void main() {
+  Application app = new Application("#app_container");
+}
