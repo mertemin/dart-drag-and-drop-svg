059d0a4cc543b228b0d3cc65a3629e1d3ec70951
Mert Emin Kalender <merteminkalender@gmail.com>
2014-01-14 22:08:48 -0800

    Dragging problem in Firefox fix

--- dart_drag_and_drop_svg.dart
+++ dart_drag_and_drop_svg.dart
@@ -3,13 +3,13 @@ import 'dart:async';
 import 'dart:svg';
 
 class BasicUnit {
-  
+
   SvgSvgElement canvas;
   GElement group;
   RectElement body;
   bool dragging;
   num dragOffsetX, dragOffsetY, width, height;
-  
+
   BasicUnit(SvgSvgElement this.canvas, num x, num y, num this.width, num this.height) {
     this.body = new RectElement();
     this.body.setAttribute('x', '$x');
@@ -17,30 +17,30 @@ class BasicUnit {
     this.body.setAttribute('width', '$width');
     this.body.setAttribute('height', '$height');
     this.body.classes.add('processing_body');
-    
+
     this.body.onMouseDown.listen(select);
-    
+
     this.group = new GElement();
     this.group.append(this.body);
-    
+
     this.dragging = false;
   }
-  
+
   void select(MouseEvent e) {
     e.preventDefault();
     this.dragging = true;
-    
+
     var mouseCoordinates = getMouseCoordinates(e);
     this.dragOffsetX = mouseCoordinates['x'] - body.getCtm().e; //double.parse(body.attributes['x']);
     this.dragOffsetY = mouseCoordinates['y'] - body.getCtm().f;
-    
+
     this.canvas.onMouseMove.listen(moveStarted).resume();
     this.canvas.onMouseUp.listen(moveCompleted).resume();
   }
-  
+
   void moveStarted(MouseEvent e) {
     e.preventDefault();
-    
+
     if (dragging) {
       var mouseCoordinates = getMouseCoordinates(e);
       num newX = mouseCoordinates['x'] - dragOffsetX;
@@ -48,31 +48,31 @@ class BasicUnit {
       this.body.setAttribute('transform', 'translate($newX, $newY)');
     }
   }
-  
+
   void moveCompleted(MouseEvent e) {
     e.preventDefault();
     this.dragging = false;
-    
+
     this.canvas.onMouseMove.listen(moveStarted).cancel();
     this.canvas.onMouseUp.listen(moveCompleted).cancel();
   }
-  
+
   dynamic getMouseCoordinates(e) {
-    return {'x': (e.offset.x - this.canvas.currentTranslate.x)/this.canvas.currentScale, 
-            'y': (e.offset.y - this.canvas.currentTranslate.y)/this.canvas.currentScale};
+    return {'x': (e.client.x - this.canvas.currentTranslate.x)/this.canvas.currentScale,
+            'y': (e.client.y - this.canvas.currentTranslate.y)/this.canvas.currentScale};
   }
 }
 
 class Application {
   int WIDTH = 80, HEIGHT = 60;
-  
+
   SvgSvgElement canvas;
-  
+
   Application(canvas_id) {
     this.canvas = document.querySelector(canvas_id);
     this.canvas.onDoubleClick.listen((MouseEvent e) => addNewUnit(e));
   }
-  
+
   void addNewUnit(MouseEvent e) {
     num x = e.offset.x - WIDTH/2;
     num y = e.offset.y - HEIGHT/2;
